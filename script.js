6ecc028d85cc467ca47c9ab40f87b4b62222be54
amanicps <80164449+AmaniMango@users.noreply.github.com>
2025-04-03 14:27:18 -0500

    deleted random empty lines in HTML+CSS+JS

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let header = document.querySelector("h1");
 let button = document.querySelector(".submit-btn");
-
-
 button.addEventListener("mouseover", function(){
     header.innerHTML = "Thank You for Signing Up!";
 });
